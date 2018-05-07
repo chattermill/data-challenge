f8ee9f1d807b82ad9c484cd8468e6e0f857785e3
dindatisi <dindatcalista@veritrans.co.id>
2018-05-07 18:58:16 +0100

    add viz for money_spent

--- viz.py
+++ viz.py
@@ -7,8 +7,17 @@ df = pd.read_csv(file,index_col=False)
 print(df.head())
 print(df.describe())
 
+
 apps_bought = df.app_bought.unique().tolist()
 num_count = df.app_bought.value_counts()
-
+plt.subplot(2,1,1)
+plt.title('Apps Bought')
 plt.bar(apps_bought,num_count)
+
+money_spent = df.money_spent.unique().tolist()
+spending_count = df.money_spent.value_counts()
+plt.subplot(2,1,2)
+plt.title('Money Spent')
+plt.bar(money_spent,spending_count)
+
 plt.show()
