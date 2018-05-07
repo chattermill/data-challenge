8212113962ba1c6be7e718464cd8f215ebeae081
dindatisi <dindatcalista@veritrans.co.id>
2018-05-07 20:54:59 +0100

    fix wrong date parsing, update cleaned file

--- cleaning.py
+++ cleaning.py
@@ -5,7 +5,7 @@ import pandas as pd
 file = "reddit_exercise_data.csv"
 
 def generate_buckets(data):
-	data['apps_bought_bucket'] = pd.cut(data.app_bought, [0,20,40,60,500], include_lowest=True,labels = ['<20','20-40','40-60','>60'])
+	data['apps_bought_bucket'] = pd.cut(data.app_bought, [0,20,60,500], include_lowest=True,labels = ['<20','20-60','>60'])
 	data['money_spent_bucket'] = pd.qcut(data.money_spent,5,labels=["1st_quantile","2nd_quantile","3rd_quantile","4th_quantile","5th_quantile"])
 	data['apps_bought_bucket'] = data.apps_bought_bucket.astype(str)
 	data['money_spent_bucket'] = data.money_spent_bucket.astype(str)
@@ -13,7 +13,7 @@ def generate_buckets(data):
 
 def clean_date(data):
 	dates = data.date.values.tolist()
-	dates_clean = [pd.to_datetime(date) for date in dates]
+	dates_clean = [pd.to_datetime(date,yearfirst=True) for date in dates]
 	data['date'] = dates_clean
 	# remove the time in datetime since every row has same value 0:0:0
 	data['date'] = data.date.dt.date
